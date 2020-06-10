516c37e95df149275408c0192ade2eb2d6c58d42
Bishoy Georgy <bishoy.georgy@incorta.com>
2020-06-10 20:40:28 +0200

    first commit

---
+++ kmama/src/main/java/com/learning/kmama/KmamaApplication.java
@@ -0,0 +1,13 @@
+package com.learning.kmama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KmamaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KmamaApplication.class, args);
+	}
+
+}
---
+++ kmama/src/test/java/com/learning/kmama/KmamaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learning.kmama;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KmamaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
